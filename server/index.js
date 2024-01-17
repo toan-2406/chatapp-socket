41c4ea50342788494d5be46f0a31cc451d1e7636
sev7n <ntoan2884@gamil.com>
2024-01-17 10:18:33 +0700

    redeloy

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://chatapp-socket-smoky.vercel.app/',
         methods: ['GET', 'POST']
     }
 });
