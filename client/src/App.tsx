1f31a91efbfba575ebc1701bb295dfd30fb4fb72
sev7n <ntoan2884@gamil.com>
2024-01-17 17:30:43 +0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,58 +1,40 @@
 import { useEffect, useId, useState } from 'react'
 import './App.css'
 import io from 'socket.io-client'
-import ChatComponent from './components/Chat'
-const socket = io('http://localhost:3000')
+import ListRoomComponent from './components/ListRoom'
+import CreateRoom from './components/CreateRoom'
 
 function App() {
   const id = useId()
   const [message, setMessage] = useState('')
   const [room, setRoom] = useState('')
   const [messageHistory, setMessageHistory] = useState<string[]>([]);
-  const sendMessage = () => {
-    socket.emit('send_message', { message, room,id })
-  }
 
-  const sendJoinRoom = () => {
-    if (room !== "") {
-      socket.emit('join_room', room)
+  const joinRoom = async () => {
+    try {
+      const apiResponse = await fetch('http://localhost:5000/api/v1/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          room,
+        }),
+      });
+      console.log(apiResponse)
+    } catch (error:any) {
+      console.error('Error:', error.message);
+      console.log('An error occurred while joining the room.');
     }
-  }
+  };
 
-  useEffect(() => {
-    // Sử dụng event 'receive_message_history' để nhận lịch sử tin nhắn khi tham gia vào phòng
-    socket.on('receive_message_history', (data) => {
-      setMessageHistory(data.history);
-    });
 
-    // Sử dụng event 'receive_message' để nhận tin nhắn mới
-    socket.on('receive_message', (data) => {
-      setMessageHistory(prevHistory => [...prevHistory, data.message]); // Thêm tin nhắn mới vào lịch sử
-    });
 
-    // Cleanup effect khi component unmount
-    return () => {
-      socket.off('receive_message_history');
-      socket.off('receive_message');
-    }
-  }, [room]);
 
   return (
     <div className='App'>
-      <div>
-        <input placeholder='Enter room...' value={room} onChange={(e) => setRoom(e.target.value)} />
-        <button onClick={sendJoinRoom}>Join Room</button>
-      </div>
-      <div>
-        <input type="text" placeholder='Enter message...' value={message} onChange={(e) => setMessage(e.target.value)} />
-        <button onClick={sendMessage}>Send Message</button>
-      </div>
-      <h1>Message:</h1>
-      {/* Hiển thị lịch sử tin nhắn */}
-      {messageHistory.map((msg, index) => (
-        <div key={index}>{msg}</div>
-      ))}
-       <ChatComponent />
+       <ListRoomComponent />
+       <CreateRoom/>
     </div>
   )
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
-
+import { v4 as uuidv4 } from 'uuid';
 import * as middlewares from './middlewares';
 import api from './api';
 import MessageResponse from './interfaces/MessageResponse';
+import http from "http";
+import * as chat from "./api/chat"; 
+import chatRouter from './routers/ChatRouter'
 
 require('dotenv').config();
 
@@ -15,10 +18,14 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+const server = http.createServer(app);
+
+const manager = chat.initChat(server);
+app.use('/api/v1/chat', chatRouter);
 
 app.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
   });
 });
 
